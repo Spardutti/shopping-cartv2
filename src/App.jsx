ae635c478af5775542aa9af609c06acdba90d600
spardutti <luisdamian.sp@gmail.com>
2021-01-29 19:06:41 -0300

    fixed some routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -23,24 +23,25 @@ const App = () => {
         return e.name;
       })
       .indexOf(name);
-      console.log(index)
+    console.log(index);
     //push element to cartItems
     setCartItems([...cartItems, imgData[index]]);
   };
 
-
   const deleteItem = (e) => {
     let divParent = e.target.parentNode.parentNode;
     let name = divParent.childNodes[0].innerHTML;
-    let index = cartItems.map((e) => {
-      return e.name
-    }).indexOf(name)
+    let index = cartItems
+      .map((e) => {
+        return e.name;
+      })
+      .indexOf(name);
     let arr = cartItems;
-    arr.splice(index, 1)
-    setCartItems(arr)
-    console.log(cartItems)
+    arr.splice(index, 1);
+    setCartItems(arr);
+    console.log(cartItems);
     setCount(count - 1);
-  }
+  };
   //update the checkout amout
   const updateTotal = () => {
     cartItems.map((e) => setTotal(total + e.price));
@@ -51,8 +52,6 @@ const App = () => {
     setCount(cartItems.length);
     updateTotal();
   }, [cartItems]);
-
- 
 
   return (
     <BrowserRouter>
@@ -78,16 +77,23 @@ const App = () => {
       </div>
 
       <Switch>
-        <Route  path="/" render={() => <Home addToCart={addToCart} />} />
+    
         <Route
           exact
           path="/cart"
-          render={() => <Cart cartItems={cartItems} total={total} delete={deleteItem} />}
+          render={() => (
+            <Cart cartItems={cartItems} total={total} delete={deleteItem} />
+          )}
         />
         <Route
           exact
           path="/shop"
           render={() => <Shop click={addToCart} imgData={imgData} />}
+        />
+            <Route
+          
+          path="/"
+          render={() => <Home addToCart={addToCart} />}
         />
       </Switch>
     </BrowserRouter>
